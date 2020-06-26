9516626924439fd355a4ba0079cfe9cd102da191
KhamZero <khamzat99@gmail.com>
2020-06-26 18:15:24 +0300

    Add a method to analyze how many macros a file has. Also returns a dictionary with numbers of these macros.

--- doc_analyzer.py
+++ doc_analyzer.py
@@ -5,12 +5,15 @@ from oletools.olevba import VBA_Parser, TYPE_OLE, TYPE_OpenXML, TYPE_Word2003_XM
 
 def main():
     filename = readfile_from_documents()
-    checkfile(filename)
-    macros = extract_macros(filename)  # Возвращает все входящие в файл макросы
-    print(macros)
-
+    if not is_file_has_VBA_macros(filename):
+        return 
+    code_items = get_items(filename)  # Возвращает словарь с данными о входящих в документ макросах
+    print(code_items)
+    
+    
 
 def readfile_from_documents():
+    """Return file path from directory and input."""
     filename = input("Input file name to analysis: ")
     base_path = Path(__file__).parent
     directory_path = (base_path / "documents").resolve()
@@ -18,29 +21,33 @@ def readfile_from_documents():
     return(file_path)
 
 
-def checkfile(filename):
-    vbparser = VBA_Parser(filename)
-    print('The file type is "%s"' % (vbparser.type))
-    if vbparser.detect_vba_macros():
+def is_file_has_VBA_macros(filename):
+    vbaparser = VBA_Parser(filename)
+    print('The file type is "%s"' % (vbaparser.type))
+    if vbaparser.detect_vba_macros():
         print("VBA Macros найден!")
+        print('- '*39)
+        return True
     else:
         print("VBA Macros не найден!")
-
-
-def extract_macros(filename):
-    vbaparser = VBA_Parser(filename)
-    for (filename, stream_path, vba_filename, vba_code) in vbaparser.extract_macros():
-        print('-'*79)
-        print('Filename    :', filename)
-        print('OLE stream  :', stream_path)
-        print('VBA filename:', vba_filename)
         print('- '*39)
-        print(vba_code)  # Все это не нужно, оставим только return
-        return vba_code
+        return False
+
+
+def get_items(filename):
+    vbaparser = VBA_Parser(filename)
+    vbaparser.analyze_macros()
+    print ('Ключевые слова автоматического вызова: %d' % vbaparser.nb_autoexec)
+    print ('Подозрительные ключевые слова: %d' % vbaparser.nb_suspicious)
+    print ('IOCs: %d' % vbaparser.nb_iocs)
+    print ('Шестнадцатеричные обфусцированные строки: %d' % vbaparser.nb_hexstrings)
+    print ('Base64 обфусцированные строки: %d' % vbaparser.nb_base64strings)
+    print ('Dridex обфусцированные строки: %d' % vbaparser.nb_dridexstrings)
+    print ('VBA обфусцированные строки: %d' % vbaparser.nb_vbastrings)
+    print('- '*39)
+    return({'AutoExec': vbaparser.nb_autoexec, 'Dridex': vbaparser.nb_dridexstrings, 'VBA': vbaparser.nb_vbastrings})
+    
 
 
 if __name__ == "__main__":
     main()
-
-
-input()
